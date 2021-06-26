e2ef89e91292a30606a3e79fead4a01a1b019bbb
izzuddin991 <izzuddin.muhamad@yahoo.com>
2021-06-26 08:17:44 +0800

    mirror smart farming

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //Install express server
 const express = require('express');
-const path = require('path');
+
 
 const app = express();
 
+app.use(express.static('./package.json'));
+
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/iz-property'));
+// app.use(express.static('./dist/iz-property'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/iz-property/'}),
